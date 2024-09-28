ab60449d431bde2a3c99357f9ba09dccd04cceb3
apvjsme <anhphavovan@gmail.com>
2024-09-28 23:48:53 +0700

    structure

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import "./App.scss";
 import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world</p>
         <a
